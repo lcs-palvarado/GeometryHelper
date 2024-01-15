932c5258b2d1d4f51ee0e8e8a6d2e7bec291e755
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-15 12:15:18 -0500

    Added a odel file to define circle.

---
+++ GeometryHelper/Model/Circle.swift
@@ -0,0 +1,28 @@
+//
+//  Circle.swift
+//  GeometryHelper
+//
+//  Created by Pablo Alvarado on 15/01/24.
+//
+
+import Foundation
+
+
+struct Circle {
+    
+    // MARK: Stored properties
+    var radius: Double
+    
+    // MARK: Computed properties
+    var diameter: Double {
+        return 2 * radius
+    }
+    
+    var perimeter: Double {
+        return 2 * Double.pi * radius
+    }
+    
+    var area: Double{
+        return Double.pi * radius * radius
+    }
+}
