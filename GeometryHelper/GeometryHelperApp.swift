20fe664f6bdc3c98b4b27f74fce07a6abe01a653
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-15 11:25:40 -0500

    Initial Commit

---
+++ GeometryHelper/GeometryHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryHelperApp.swift
+//  GeometryHelper
+//
+//  Created by Pablo Alvarado on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GeometryHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
