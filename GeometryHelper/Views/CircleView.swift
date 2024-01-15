a6902a8951662a57d1b1b7bcae709690be3aa9e9
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-15 11:54:47 -0500

    Organized project by adding Views and Model groups and a new file named CircleView.

---
+++ GeometryHelper/Views/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  GeometryHelper
+//
+//  Created by Pablo Alvarado on 15/01/24.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
