e6df26f2a48f816e1d50bcfceb79245ed1f58dcb
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-15 12:12:16 -0500

    Added a slider to let the user change the radius of the circle.

--- GeometryHelper/Views/CircleView.swift
+++ GeometryHelper/Views/CircleView.swift
@@ -8,7 +8,18 @@
 import SwiftUI
 
 struct CircleView: View {
+    
+  // Mark: Stored properties
+    @State var radius = 10.0
+    // Mark: computed properties
     var body: some View {
+        var body: some View {
+            Text("\(radius)")
+                .font(.largeTitle)
+            
+            Slider(value: $tradius, in: 0...100)
+        }
+        .padding()
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
